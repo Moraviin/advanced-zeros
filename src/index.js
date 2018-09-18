1b7108a62938e8132024ecd947d6b77ab6781c22
Владимир Зубович <moraviin@gmail.com>
2018-09-18 22:03:37 +0300

    98/100

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let maxSimpleMutiplier=2;
+  let copyBase=base;
+  let zerosCount=0;
+  let extraDivide=0;
+  while (copyBase!=1){
+    if(copyBase%maxSimpleMutiplier==0){
+      copyBase/=maxSimpleMutiplier;
+      extraDivide++;
+    }
+    else {
+      maxSimpleMutiplier++;
+      extraDivide=0;
+    }
+  }
+  
+ while(number>0){
+   number/=maxSimpleMutiplier;
+   number=Math.floor(number)
+   zerosCount+=number;
+ }
+ return Math.floor(zerosCount/extraDivide); // your implementation
 }
